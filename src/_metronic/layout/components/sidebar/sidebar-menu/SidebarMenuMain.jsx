754ed779ee11852fc8c83448c4faa4353114b838
Akshaypareek01 <akshay96102@gmail.com>
2024-05-07 12:48:15 +0530

    working

---
+++ src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.jsx
@@ -0,0 +1,79 @@
+import { useIntl } from "react-intl";
+import { SidebarMenuItem } from "./SidebarMenuItem";
+import PeopleIcon from "@mui/icons-material/People";
+import WidgetsIcon from "@mui/icons-material/Widgets";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CategoryIcon from "@mui/icons-material/Category";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import PaymentsIcon from "@mui/icons-material/Payments";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import WorkIcon from '@mui/icons-material/Work';
+const SidebarMenuMain = () => {
+  // const {userPermisson}=useContext(UserContext);
+  const userPermisson = JSON.parse(sessionStorage.getItem("userPermisson"));
+  const intl = useIntl();
+
+  return (
+    <>
+      <SidebarMenuItem
+        to="/dashboard"
+        icon={<WidgetsIcon style={{ color: "orange", fontSize: "25px" }} />}
+        title={intl.formatMessage({ id: "MENU.DASHBOARD" })}
+        fontIcon="bi-app-indicator"
+      />
+
+      <SidebarMenuItem
+        to="/team"
+        icon={<PeopleIcon style={{ color: "orange", fontSize: "25px" }} />}
+        title="Team"
+        fontIcon="bi-layers"
+      />
+
+<SidebarMenuItem
+        to="/client"
+        icon={
+          <WorkIcon style={{ color: "orange", fontSize: "25px" }} />
+        }
+        title="Client"
+        fontIcon="bi-layers"
+      />
+
+   
+
+<SidebarMenuItem
+        to="/jobs"
+        icon={
+          <ShoppingCartIcon style={{ color: "orange", fontSize: "25px" }} />
+        }
+        title="Jobs"
+        fontIcon="bi-layers"
+      />
+
+      <SidebarMenuItem
+        to="/inventory"
+        icon={<CategoryIcon style={{ color: "orange", fontSize: "25px" }} />}
+        title="Inventory"
+        fontIcon="bi-layers"
+      />
+
+
+  
+
+      <SidebarMenuItem
+        to="/accounts"
+        icon={
+          <ManageAccountsIcon style={{ color: "orange", fontSize: "25px" }} />
+        }
+        title="Accounts"
+        fontIcon="bi-layers"
+      />
+
+
+
+     
+    </>
+  );
+};
+
+export { SidebarMenuMain };
---
+++ src/app/routing/PrivateRoutes.jsx
@@ -0,0 +1,287 @@
+import { FC, Suspense, useContext} from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import {MasterLayout} from '../../_metronic/layout/MasterLayout'
+import TopBarProgress from 'react-topbar-progress-indicator'
+import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
+import {MenuTestPage} from '../pages/MenuTestPage'
+import {getCSSVariableValue} from '../../_metronic/assets/ts/_utils'
+
+import UserContext from '../../Context/UserContext'
+
+import {  Client } from '../pages/ManageB2BUsers/Client'
+
+import {  ClientAdd } from '../pages/ManageB2BUsers/ClientAdd'
+import { Team } from '../pages/Vendors/Team'
+import { Rates } from '../pages/Rates/Rates'
+import { Orders } from '../pages/Orders/Orders'
+import {  Inventory } from '../pages/Categories/Inventory'
+
+
+import { Accounts } from '../pages/Accounts/Accounts'
+import { Users } from '../pages/Users/Users'
+import {  ViewInventory } from '../pages/Categories/ViewInventory'
+
+import { AddTeam } from '../pages/Vendors/AddTeam'
+import { TeamView } from '../pages/Vendors/TeamView'
+import { B2BOrders } from '../pages/Orders/B2BOrders'
+import { AddB2BOrder } from '../pages/Orders/AddB2BOrder'
+import { AddUsers } from '../pages/Users/AddUsers'
+import { AddOrder } from '../pages/Orders/AddOrders'
+
+import { ClientView } from '../pages/ManageB2BUsers/ClientView'
+import { CreateInvoice } from '../pages/Accounts/CreateInvoiceAcc'
+import { Invoices } from '../pages/Accounts/Invoices'
+import { AllInvoices } from '../pages/Accounts/AllInvoices'
+
+
+
+
+
+
+
+
+
+
+const PrivateRoutes = () => {
+  const {userPermisson}=useContext(UserContext);
+
+  return (
+    <Routes>
+      <Route element={<MasterLayout />}>
+  
+        
+        <Route path='auth/*' element={<Navigate to='/dashboard' />} />
+   
+        <Route path='dashboard' element={<DashboardWrapper />} />
+      
+        <Route path='menu-test' element={<MenuTestPage />} />
+     
+       
+        <Route
+          path='team/*'
+          element={
+            <SuspensedView>
+              <Team />
+            </SuspensedView>
+          }
+        />
+
+<Route
+          path='team/add/*'
+          element={
+            <SuspensedView>
+              <AddTeam />
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='team/view/:id'
+          element={
+            <SuspensedView>
+              <TeamView />
+            </SuspensedView>
+          }
+        />
+
+{/* <Route
+          path='rates/*'
+          element={
+            <SuspensedView>
+              <Rates />
+            </SuspensedView>
+          }
+        /> */}
+
+<Route
+          path='orders/*'
+          element={
+            <SuspensedView>
+              <Orders />
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='orders/add/*'
+          element={
+            <SuspensedView>
+              <AddOrder />
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='jobs/*'
+          element={
+            <SuspensedView>
+              <B2BOrders />
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='jobs/add/*'
+          element={
+            <SuspensedView>
+              <AddB2BOrder />
+            </SuspensedView>
+          }
+        />
+
+<Route
+          path='inventory/*'
+          element={
+            <SuspensedView>
+              <Inventory />
+            </SuspensedView>
+          }
+        />
+
+<Route
+          path='inventory/view-categorie/:id'
+          element={
+            <SuspensedView>
+              <ViewInventory />
+            </SuspensedView>
+          }
+        />
+
+{/* <Route
+          path='sales/*'
+          element={
+            <SuspensedView>
+              <Sales />
+            </SuspensedView>
+          }
+        /> */}
+        <Route
+          path='accounts/create-invoice/*'
+          element={
+            <SuspensedView>
+              <CreateInvoice />
+            </SuspensedView>
+          }
+        />
+
+<Route
+          path='accounts/invoice-view/*'
+          element={
+            <SuspensedView>
+              < AllInvoices/>
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='accounts/invoice-view/view/:id'
+          element={
+            <SuspensedView>
+              <Invoices />
+            </SuspensedView>
+          }
+        />
+     
+     
+      
+        {/* <Route
+          path='sales/sales-view/*'
+          element={
+            <SuspensedView>
+              <SalesView />
+            </SuspensedView>
+          }
+          /> */}
+          {/* <Route
+          path='sales/sales-view/view/:id'
+          element={
+            <SuspensedView>
+              <Invoices />
+            </SuspensedView>
+          }
+          /> */}
+{/* 
+<Route
+          path='plans/*'
+          element={
+            <SuspensedView>
+              <Plans />
+            </SuspensedView>
+          }
+        /> */}
+
+<Route
+          path='accounts/*'
+          element={
+            <SuspensedView>
+              <Accounts />
+            </SuspensedView>
+          }
+        />
+
+{/* <Route
+          path='users/*'
+          element={
+            <SuspensedView>
+              <Users />
+            </SuspensedView>
+          }
+        />
+
+<Route
+          path='users/users_add/*'
+          element={
+            <SuspensedView>
+              <AddUsers />
+            </SuspensedView>
+          }
+        /> */}
+  
+
+<Route
+          path='client/*'
+          element={
+            <SuspensedView>
+              <Client />
+            </SuspensedView>
+          }
+        />
+        <Route
+          path='client_add/*'
+          element={
+            <SuspensedView>
+              <ClientAdd />
+            </SuspensedView>
+          }
+        />
+
+     <Route
+          path='client_view/:id/*'
+          element={
+            <SuspensedView>
+              <ClientView />
+            </SuspensedView>
+          }
+        />
+
+
+
+
+
+
+        {/* Page Not Found /error/404 */}
+
+        <Route path='*' element={<Navigate to='/' />} />
+      </Route>
+    </Routes>
+  )
+}
+
+const SuspensedView = ({children}) => {
+  const baseColor = getCSSVariableValue('--kt-primary')
+  TopBarProgress.config({
+    barColors: {
+      '0': baseColor,
+    },
+    barThickness: 1,
+    shadowBlur: 5,
+  })
+  return <Suspense fallback={<TopBarProgress />}>{children}</Suspense>
+}
+
+export {PrivateRoutes}
